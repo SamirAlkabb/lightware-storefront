6a04cd592f3865b20fd5dc27bc632c37e546a7d5
deploy <deploy@lightware.local>
2026-09-07 16:39:53 +0300

    deploy 2026-09-07 16:39

--- assets/xr--D1Kj97z.js
+++ assets/xr--D1Kj97z.js
@@ -1,4 +1,4 @@
-import{c as Ll,d as Dl}from"./index-BK6Lt_eC.js";/**
+import{c as Ll,d as Dl}from"./index-2y75u17J.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
