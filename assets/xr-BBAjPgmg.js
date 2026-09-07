bb2955e070411519a55133a9928556d7480c7222
deploy <deploy@lightware.local>
2026-09-07 17:40:53 +0300

    deploy 2026-09-07 17:40

--- assets/xr-BBAjPgmg.js
+++ assets/xr-BBAjPgmg.js
@@ -1,4 +1,4 @@
-import{c as Ll,d as Dl}from"./index-Bbgo4V6A.js";/**
+import{c as Ll,d as Dl}from"./index-Dqm293r8.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
