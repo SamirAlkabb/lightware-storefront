345b3e4e9bfc7ea653d125504c056c85d14581fc
deploy <deploy@lightware.local>
2026-09-08 23:50:45 +0300

    deploy 2026-09-08 23:50

--- assets/xr-CUcZLkzS.js
+++ assets/xr-CUcZLkzS.js
@@ -1,4 +1,4 @@
-import{c as Ll,d as Dl}from"./index-Dqm293r8.js";/**
+import{c as Ll,d as Dl}from"./index-DkbBb8FG.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
