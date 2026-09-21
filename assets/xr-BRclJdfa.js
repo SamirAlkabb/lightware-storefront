c9325b02bcbbff486f7caae90198216dd36bb3b9
deploy <deploy@lightware.local>
2026-09-21 13:11:33 +0300

    deploy 2026-09-21 13:11

--- assets/xr-BRclJdfa.js
+++ assets/xr-BRclJdfa.js
@@ -1,4 +1,4 @@
-import{c as Ll,d as Dl}from"./index-CoDpy1T2.js";/**
+import{c as Ll,d as Dl}from"./index-DzplAHkg.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
